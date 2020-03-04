fa39e8c433847149140b9f61c4f4c1dddd057b9b
Simoid <simonkawaji@gmail.com>
2020-03-04 23:37:43 +0100

    Test commit with some code

---
+++ modsim-unity-project/Assets/Scripts/Particle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public GameObject gameObject;
+    public Vector3 pos;
+    public Vector3 vel;
+    public Vector3 combinedForce;
+    public float density;
+    public float pressure;
+}
---
+++ modsim-unity-project/Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    float calculateDensity(Particle particle, Particle[] particles){
+        return 0;
+    }
+
+    float calculatePoly6Kernel(float r, float h){
+        float abs_r = Mathf.Abs(r);
+
+        if(0 <= abs_r && abs_r <= h ){
+            return Mathf.Pow(Mathf.Pow(h, 2) - Mathf.Pow(abs_r, 2), 3);
+        }else{
+            return 0;
+        }
+    }
+}
